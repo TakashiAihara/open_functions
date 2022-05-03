103ca3993d7cb1e7cf65d9398f24a7cd54309696
TakashiAihara <takashi.aihara.0523@gmail.com>
2022-05-03 10:36:44 +0900

    fix bug

--- get_chromedriver.sh
+++ get_chromedriver.sh
@@ -1,12 +1,12 @@
-get_chromedriver_latest(){
+get_chromedriver_latest_version(){
   curl -sS "chromedriver.storage.googleapis.com/LATEST_RELEASE"
 }
 
-get_chromedriver(){
-  curl https://chromedriver.storage.googleapis.com/$(get_chromedriver_latest)/chromedriver_linux64.zip -o /tmp/chromedriver.zip
+get_chromedriver_latest(){
+  curl https://chromedriver.storage.googleapis.com/$(get_chromedriver_latest_version)/chromedriver_linux64.zip -o /tmp/chromedriver.zip
   unzip /tmp/chromedriver.zip -d /usr/local/bin/
   chmod +x /usr/local/bin/chromedriver
-  rm /tmp/chromedriver
+  rm /tmp/chromedriver.zip
 }
 
-get_chromedriver
+get_chromedriver_latest
